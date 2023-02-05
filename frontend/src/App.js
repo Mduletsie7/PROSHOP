7a4449f1c5f70505f28ccc445fb0e6f5526478f9
Mduletsie7 <mdu.letsie7@gmail.com>
2023-02-05 10:55:08 +0200

    Added Router to Header & Product links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { Container } from "react-bootstrap"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import Homescreen from "./screens/Homescreen"
+import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
 
 const App = () => {
   return (
@@ -12,7 +13,8 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Routes>
-            <Route path='/' element={<Homescreen />} />
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
           </Routes>
         </Container>
       </main>
--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -3,7 +3,7 @@ import { Row, Col } from "react-bootstrap"
 import Product from "../components/Product"
 import products from "../products"
 
-const Homescreen = () => {
+const HomeScreen = () => {
   return (
     <>
       <h1>Latest Products</h1>
@@ -18,4 +18,4 @@ const Homescreen = () => {
   )
 }
 
-export default Homescreen
+export default HomeScreen
