9a0c6b389a811c85cfc201b31b25ccb0a9b8e744
Mduletsie7 <mdu.letsie7@gmail.com>
2023-02-05 10:41:20 +0200

    Successful homescreen Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Homescreen from "./screens/Homescreen";
+import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Homescreen from "./screens/Homescreen"
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <Homescreen />
+          <Routes>
+            <Route path='/' element={<Homescreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
-  );
-};
+    </Router>
+  )
+}
 
-export default App;
+export default App
