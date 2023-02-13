d317a0f86415d8589edf0e517d369d7ef22ea4b0
Mduletsie7 <mdu.letsie7@gmail.com>
2023-02-13 11:05:49 +0200

    Created reducers and Loaders for CartScreen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,14 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Row, Col, ListGroup, Image, Form, Button, Card } from "react-bootstrap"
+import Message from "../components/Message"
+import { addToCart } from "../actions/cartActions"
+import { useParams } from "react-router-dom"
 
-const CartScreen = () => {
-  return <div>Cart</div>
+const CartScreen = ({ location, history }) => {
+  const productId = useParams()
+
+  const qty = location.search
 }
 
 export default CartScreen
